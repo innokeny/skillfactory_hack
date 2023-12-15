a9dfe637f996a02f64c70139255701dc1b488eb1
rar7 <103815222+innokeny@users.noreply.github.com>
2023-12-16 00:38:08 +0700

    Update front.py

--- front.py
+++ front.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import pickle
 
-model = pickle.load(open('trained_model.pkl', 'rb'))
+#model = pickle.load(open('trained_model.pkl', 'rb'))
 
-def predict(input_data):
-    # Логика прогнозирования с использованием модели
-    prediction = model.predict(input_data)
-    return prediction
+#def predict(input_data):
+#    # Логика прогнозирования с использованием модели
+#    prediction = model.predict(input_data)
+#     return prediction
 
 def main():
     st.title('Мой приложение с моделью')
