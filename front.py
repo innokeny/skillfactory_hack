506a8c157e2bcf9d6f4dc1afa1be0c7febad6a36
rar7 <103815222+innokeny@users.noreply.github.com>
2023-12-16 00:32:33 +0700

    Add files via upload

---
+++ front.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pickle
+
+model = pickle.load(open('trained_model.pkl', 'rb'))
+
+def predict(input_data):
+    # Логика прогнозирования с использованием модели
+    prediction = model.predict(input_data)
+    return prediction
+
+def main():
+    st.title('Мой приложение с моделью')
+    # Добавьте остальные элементы интерфейса и логику обработки пользовательского ввода здесь
+    
+if __name__ == '__main__':
+    main()
